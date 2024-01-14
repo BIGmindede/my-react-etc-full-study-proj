2e9ee506d50d83f91fdd60b9d8c2bc2bd081f949
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-01-15 02:23:43 +0300

    WAY-12: fix of whatever

--- src/shared/config/storyBook/TranslationDecorator.tsx
+++ src/shared/config/storyBook/TranslationDecorator.tsx
@@ -1,11 +1,9 @@
 import { type StoryFn } from '@storybook/react'
 import { I18nextProvider } from 'react-i18next'
-import i18nForTests from '../i18n/i18nForStoryBook'
+import i18nForStoryBook from 'shared/config/i18n/i18nForStoryBook'
 
-export const TranslationDecorator = (StoryComponent: StoryFn) => {
-    return (
-        <I18nextProvider i18n={i18nForTests}>
-            <StoryComponent/>
-        </I18nextProvider>
-    )
-}
+export const TranslationDecorator = (StoryComponent: StoryFn) => (
+    <I18nextProvider i18n={i18nForStoryBook}>
+        <StoryComponent />
+    </I18nextProvider>
+)
