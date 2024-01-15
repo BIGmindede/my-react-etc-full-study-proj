6006a01910b0b6266dfcfde00a456b640ca69e33
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-01-15 03:07:55 +0300

    WAY-12: rollback  diffs

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,9 +1,11 @@
 import i18next from 'i18next'
+import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
 import I18nextHttpBackend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
 i18next
     .use(I18nextHttpBackend)
+    .use(I18nextBrowserLanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: 'ru',
